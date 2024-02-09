9b243d4ad76b167358f89824b6fe6db39ce8314f
LokeshKodam <lokeshkodam@gmail.com>
2024-02-09 14:54:24 -0800

    d2.5

--- src/OpticalMarkReaderMain.java
+++ src/OpticalMarkReaderMain.java
@@ -13,25 +13,49 @@ public class OpticalMarkReaderMain {
     public static void main(String[] args) {
         String pathToPdf = fileChooser();
         System.out.println("Loading pdf at " + pathToPdf);
+        int answersheetCount = 12;
+        int answerCount1 = 0;
+        int answerCount2 = 0;
+        int answerCount3 = 0;
+        int answerCount4 = 0;
+        int answerCount5 = 0;
+
 
         ArrayList<PImage>img = PDFHelper.getPImagesFromPdf("assets/omrtest.pdf");
+        ArrayList<PImage>twomage=PDFHelper.getPImagesFromPdf("assets/omrtest.pdf");
 
 
 
 
         DImage a = new DImage(img.get(3));
         DImage aSheet= new DImage(img.get(1));
-        short [][] answerSheet=aSheet.getBWPixelGrid();
+        short [][] answerSheet= aSheet.getBWPixelGrid();
         short[][] blackgrid = a.getBWPixelGrid();
 
-        System.out.println(blackgrid[1][1]);
+       // System.out.println(blackgrid[1][1]);
 
-        for (int i=0; i <blackgrid.length;i++) {
+        System.out.println("size of blackgrd: " + blackgrid.length + " "  + blackgrid[0].length);
+        System.out.println("size of answersheet: " + answerSheet.length +  " " + answerSheet[0].length);
+       for (int i=0; i <blackgrid.length;i++) {
             for (int j=0; j<blackgrid[0].length; j++){
+                System.out.println("i: " + i + " j: " + j);
+
                 if (blackgrid[i][j] == answerSheet[i][j]){
+                    if (blackgrid[i][j]==255){
+                        answerCount1++;
+                        i+=50;
+                    }
                 }
             }
         }
+        System.out.println(blackgrid.length+", "+blackgrid[0].length);
+        System.out.println(answerSheet.length+", "+answerSheet[0].length);
+//        System.out.println("REAL ANSWER COUNT OUT OF 12: " + answersheetCount+"/12");
+//        System.out.println("SCANTRON 1 COUNT : " + answerCount1);
+//        System.out.println("SCANTRON 2 COUNT : " + answerCount2);
+//        System.out.println("SCANTRON 3 COUNT : " + answerCount3);
+//        System.out.println("SCANTRON 4 COUNT : " + answerCount4);
+//        System.out.println("SCANTRON 5 COUNT : " + answerCount5);
 
 
 
